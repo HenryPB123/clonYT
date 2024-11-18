a99ce6e523962bdf4d4b6027b7827497277b5ea0
Henry P B <mahe2di@gmail.com>
2024-11-17 19:29:43 -0500

    Ending CSS

--- client/src/components/Menu.tsx
+++ client/src/components/Menu.tsx
@@ -119,10 +119,15 @@ const Menu: React.FC<MenuProps> = ({ darkMode, setDarkMode }) => {
         <Hr />
         <Login>
           Sign in to like videos, comment and subscribe.
-          <Button>
-            <AccountCircleOutlinedIcon />
-            SIGN IN
-          </Button>
+          <Link
+            to="signin"
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            <Button>
+              <AccountCircleOutlinedIcon />
+              SIGN IN
+            </Button>
+          </Link>
         </Login>
         <Hr />
         <Title>Best of ClonTube</Title>
